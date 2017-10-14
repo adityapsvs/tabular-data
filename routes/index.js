e25b7b206cc014db1da695f282f84b0b655e8a47
adityapsvs <adityapsvs@gmail.com>
2017-10-14 15:28:17 +0530

    Added backend for tables creation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Dumb bitch!');
+var typeMappings  =
+{
+  "String": String,
+  "Number": Number,
+  "Boolean": Boolean,
+  "ObjectId": mongoose.Schema.ObjectId
+}
+
+function makeSchema(jsonSchema) {
+  var outputSchemaDef = {}
+  for(fieldName in jsonSchema.data) {
+    var fieldType = jsonSchema.data[fieldName];
+    if(typeMappings[fieldType]) {
+      outputSchemaDef[fieldName] = typeMappings[fieldType];
+    } else {
+      console.error("invalid type specified:", fieldType);
+    }
+  }
+  return new mongoose.Schema(outputSchemaDef);
+}
+
+router.get('/', (req, res, next) => {
+  res.send('Woila!');
+})
+
+router.post('/create_table', (req, res, next) => {
+  var jsonSchema = {};
+  //Add fieldArray and dataTypeArray into jsonSchema
+  let newModel = mongoose.model(req.body.tableName, makeSchema(jsonSchema));
 });
 
 module.exports = router;
